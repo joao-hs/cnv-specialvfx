84eca01b54a728421e95f33f63896c712f53ed86
Francisco Teixeira <francisco.a.teixeira@tecnico.ulisboa.pt>
2024-05-29 19:36:25 +0000

    Upload New File

---
+++ scripts/curl_raytracer_remote.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+# Command Line arguments:
+# Mandatory:
+#   $1: Scene file with the raw content
+#   $2: Output file to save the rendered image
+# Optional:
+#   --ip: IP address of the server. Default is 127.0.0.1
+#   --port: Port number of the server. Default is 8000
+#   --texmap: Path to the texmap file. Default is not provided
+
+# Verify if the mandatory arguments are provided
+if [ $# -lt 3 ]; then
+    echo "Usage: $0 <scene_file> <output_file> <load_balancer_adress> [--texmap <texmap_file>]"
+    exit 1
+fi
+
+# Assert that scene_file and output_file are the first ones
+if [ "${1:0:2}" == "--" ]; then
+    echo "The scene file should be the first argument"
+    exit 1
+fi
+
+if [ "${2:0:2}" == "--" ]; then
+    echo "The output file should be the second argument"
+    exit 1
+fi
+
+# Assert that the scene file exists
+if [ ! -f $1 ]; then
+    echo "The scene file does not exist"
+    exit 1
+fi
+
+SCENE_FILE=$1
+OUTPUT_FILE=$2
+LOAD_BALANCER_URL=$3
+
+# Default values
+TEXMAP=""
+_DIRNAME=$(dirname $0)
+_TMP_DIR="$_DIRNAME/tmp"
+_OUT_DIR="$_DIRNAME/output"
+_PAYLOAD_FILE="$_TMP_DIR/payload.json"
+
+# Create the temporary directory
+mkdir -p $_TMP_DIR
+
+# Create the output directory
+mkdir -p $_OUT_DIR
+
+# Parse the command line arguments
+shift 3
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --texmap)
+            TEXMAP="$2"
+            shift
+            ;;
+        *)
+            ;;
+    esac
+    shift
+done
+
+
+# Add the scene file raw content to JSON
+cat $SCENE_FILE | jq -sR '{scene: .}' > $_PAYLOAD_FILE
+
+# Add texmap.bmp binary to JSON (optional step, required only for some scenes)
+if [ ! -z "$TEXMAP" ]; then
+    hexdump -ve '1/1 "%u\n"' $TEXMAP | jq -s --argjson original "$(<$_PAYLOAD_FILE)" '$original * {texmap: .}' > $_PAYLOAD_FILE
+fi
+
+# Send the request
+curl -sS -X POST "$LOAD_BALANCER_URL/raytracer?scols=400&srows=300&wcols=400&wrows=300&coff=0&roff=0&aa=false" --data @"$_PAYLOAD_FILE" > $_TMP_DIR/$OUTPUT_FILE
+
+# while response is empty, retry
+ttl=20
+while [ ! -s $_TMP_DIR/$OUTPUT_FILE ] && [ $ttl -gt 0 ]; do
+    curl -sS -X POST "$LOAD_BALANCER_URL/raytracer?scols=400&srows=300&wcols=400&wrows=300&coff=0&roff=0&aa=false" --data @"$_PAYLOAD_FILE" > $_TMP_DIR/$OUTPUT_FILE
+    ttl=$((ttl-1))
+    sleep 1
+done
+
+sed -i 's/^[^,]*,//' $_TMP_DIR/$OUTPUT_FILE
+
+base64 -d $_TMP_DIR/$OUTPUT_FILE > $_OUT_DIR/$OUTPUT_FILE
+
+
+# Remove a formatting string (remove everything before the comma)
+sed -i 's/^[^,]*,//' $_TMP_DIR/$OUTPUT_FILE
+
+# Decode from Base64
+base64 -d $_TMP_DIR/$OUTPUT_FILE > $_OUT_DIR/$OUTPUT_FILE
+
